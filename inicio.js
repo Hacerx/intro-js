613495b89b0e090348408852c9c7d9adeae54f74
Hacerx <pepesoler98@gmail.com>
2023-05-24 18:27:23 +0200

    Init

---
+++ inicio.js
@@ -0,0 +1,29 @@
+const variableNoModificable = 'Bogdan'
+
+let variableModificable = 'Bogdan'
+name = 'Bogdan'
+
+const texto = 'texto'
+const numeros = 123
+const booleano = true
+
+// Luego
+const listas = []
+const objetos = {}
+
+console.log('Hackeando la nasa...')
+console.log('Hola ' + name + '!')
+console.log(`Hola ${name}!`)
+console.log('Hackeado puto!')
+
+const n1 = 1
+const n2 = 4
+
+if(n1 > n2){
+    console.log('n1 es mayor')
+}else{
+    console.log('n2 es mayor')
+}
+
+console.log(n1 + n2)
+console.log(booleano)
